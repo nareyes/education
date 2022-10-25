a6337d9a469b2d930fc1deeebd4936a5974394c0
nareyes <nick.reyes@sebrands.com>
2022-10-25 03:52:25 -0500

    Add File via Git

---
+++ python_developer_bootcamp/class_template.py
@@ -0,0 +1,17 @@
+class NameOfClass():
+    class_attribute = 'value'
+
+    def __init__(self, param1, param2):
+        self.param1 = param1
+        self.param2 = param2
+    
+    def method(self):
+        pass
+
+    @classmethod
+    def class_method(cls, param1, param2):
+        pass
+
+    @staticmethod
+    def static_method(param1, param2):
+        pass
---
+++ python_developer_bootcamp/notes/oop/helper.py
@@ -0,0 +1,21 @@
+class Item:
+
+    @staticmethod
+    def is_integer(num):
+        '''
+        This should do something that has a relationship
+        with the class, but not something that must be
+        unique per instance.
+        '''
+        pass
+
+
+    @classmethod
+    def instantiate_from_something(cls):
+        '''
+        This should also do something that has a relationship
+        with the class. Usually these are used to manipulate 
+        different structures of data to instantiate objects, 
+        like we have done with the CSV.
+        '''
+        pass
