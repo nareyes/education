4a2918e44a05e72deea12758a00c3f9560c75e48
nareyes <nareyes@protonmail.com>
2023-01-03 11:25:55 -0600

    Add File via Git

---
+++ python_bootcamp/projects/todo_list_application/README.md
@@ -0,0 +1,6 @@
+# Todo List Application
+
+Versions:
+- CLI Application
+- GUI Application
+- Web Application
---
+++ python_bootcamp/projects/todo_list_application/main_cli.py
@@ -0,0 +1,8 @@
+# todo list cli application
+user_prompt = "Enter a Todo: "
+todo_1 = input(user_prompt)
+todo_2 = input(user_prompt)
+todo_3 = input(user_prompt)
+
+todos = [todo_1, todo_2, todo_3]
+print(todos)
