9f3b6ef36a731df913bcf5570d93b0f65a808389
nareyes <nareyes@protonmail.com>
2023-01-03 20:42:15 -0600

    Add File via Git

--- python_bootcamp/projects/todo_list_application/main_cli.py
+++ python_bootcamp/projects/todo_list_application/main_cli.py
@@ -1,20 +1,26 @@
 # todo list cli application
+user_input = "Enter a Todo: "
 todos = []
 
 while True:
-    user_action = input("Type Add, Show, or Exit: ").lower()
+    user_action = input("Type Add, Show, or Exit: ")
+    user_action = user_action.strip().lower()
 
     match user_action:
 
         case 'add':
-            todo = input("Enter a Todo: ").title()
+            todo = input(user_input).title()
             todos.append(todo)
 
         case 'show':
             for item in todos:
                 print(item)
-                
+
         case 'exit':
             break
+
+        case _:
+            print("Unknown Command")
+            
 
 print('Bye!')
