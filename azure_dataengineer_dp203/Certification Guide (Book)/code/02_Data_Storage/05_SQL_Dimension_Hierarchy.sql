dc50da6deea7df6d4e95c1d586301d509a760e9f
nareyes <nareyes@protonmail.com>
2023-01-25 13:25:14 -0600

    Add File via Git

--- azure_dataengineer_dp203/Certification Guide (Book)/code/02_Data_Storage/05_SQL_Dimension_Hierarchy.sql
+++ azure_dataengineer_dp203/Certification Guide (Book)/code/02_Data_Storage/05_SQL_Dimension_Hierarchy.sql
@@ -1,22 +1,21 @@
 -- Dimensional Hierarchy Example
 
--- DROP TABLE DimEmployee;
+DROP TABLE IF EXISTS [dbo].[DimEmployee];
 
-CREATE TABLE DimEmployee (
-	[employeeId] VARCHAR(20) NOT NULL,
-	[name] VARCHAR(100),
-	[department] VARCHAR(50),
-	[title] VARCHAR(50),
-	[parentEmployeeId] VARCHAR(20)
+CREATE TABLE [dbo].[DimEmployee] (
+	[employeeId] VARCHAR(20) NOT NULL
+	, [name] VARCHAR(100)
+	, [department] VARCHAR(50)
+	, [title] VARCHAR(50)
+	, [parentEmployeeId] VARCHAR(20)
 )
 
 GO
--- Insert some sample values
 
+-- Insert Dummy Values
 INSERT INTO [dbo].[DimEmployee] ([employeeId], [name], [department], [title], [parentEmployeeId]) VALUES (100, 'Alan Li', 'Manufacturing', 'Manager', NULL);
 INSERT INTO [dbo].[DimEmployee] ([employeeId], [name], [department], [title], [parentEmployeeId]) VALUES (200, 'Brenda Jackman', 'Manufacturing', 'Supervisor', 100);
 INSERT INTO [dbo].[DimEmployee] ([employeeId], [name], [department], [title], [parentEmployeeId]) VALUES (300, 'David Hood', 'Manufacturing', 'Machine operator', 200);
 
--- Check the hierarchy established via the [parentEmployeeId] column
+-- Check Hierarchy Established by [parentEmployeeId] Column
 SELECT * FROM [dbo].[DimEmployee];
-
