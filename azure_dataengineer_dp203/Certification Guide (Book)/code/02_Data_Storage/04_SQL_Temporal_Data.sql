72aab47bc714ca07f3fc931e9de4baeca70d66d9
nareyes <nareyes@protonmail.com>
2023-01-26 13:36:20 -0600

    Add File via Git

--- azure_dataengineer_dp203/Certification Guide (Book)/code/02_Data_Storage/04_SQL_Temporal_Data.sql
+++ azure_dataengineer_dp203/Certification Guide (Book)/code/02_Data_Storage/04_SQL_Temporal_Data.sql
@@ -1,5 +1,5 @@
 -- Temporal Data Example
-CREATE TABLE [dbo].[Customer] (
+CREATE TABLE [dbo].[DimCustomerTemp] (
   [customerId] INT NOT NULL PRIMARY KEY CLUSTERED
   , [name] VARCHAR(100) NOT NULL
   , [address] VARCHAR(100) NOT NULL
@@ -13,15 +13,15 @@ CREATE TABLE [dbo].[Customer] (
 WITH (SYSTEM_VERSIONING = ON);
 
 -- Insert Dummy Values
-INSERT INTO [dbo].[Customer] ([customerId], [name], [address], [email], [phone]) VALUES (101, 'Alan Li', '101 Test Lane, LA', 'alan@li.com', '111-222-3333');
-INSERT INTO [dbo].[Customer] ([customerId], [name], [address], [email], [phone]) VALUES (102, 'Becky King', '202 Second Lane, SF', 'becky@king.com', '222-333-4444');
-INSERT INTO [dbo].[Customer] ([customerId], [name], [address], [email], [phone]) VALUES (103, 'Daniel Martin', '303 Third Lane, NY', 'daniel@someone.com', '333-444-5555');
+INSERT INTO [dbo].[DimCustomerTemp] ([customerId], [name], [address], [email], [phone]) VALUES (101, 'Alan Li', '101 Test Lane, LA', 'alan@li.com', '111-222-3333');
+INSERT INTO [dbo].[DimCustomerTemp] ([customerId], [name], [address], [email], [phone]) VALUES (102, 'Becky King', '202 Second Lane, SF', 'becky@king.com', '222-333-4444');
+INSERT INTO [dbo].[DimCustomerTemp] ([customerId], [name], [address], [email], [phone]) VALUES (103, 'Daniel Martin', '303 Third Lane, NY', 'daniel@someone.com', '333-444-5555');
 
 -- Check Values
-SELECT * FROM Customer;
+SELECT * FROM DimCustomerTemp;
 
 -- Update A Record
-UPDATE [dbo].[Customer] SET [address] = '111 Updated Lane, LA' WHERE [customerId] = 101;
+UPDATE [dbo].[DimCustomerTemp] SET [address] = '111 Updated Lane, LA' WHERE [customerId] = 101;
 
 
 -- Validate Temporal Behavior
@@ -32,7 +32,7 @@ SELECT
    , [validFrom]
    , [validTo]
    , IIF (YEAR ([validTo]) = 9999, 1, 0) AS IsActual
-FROM dbo.Customer
-FOR SYSTEM_TIME BETWEEN ['INSERT START DATE'] AND ['INSERT END DATE']
+FROM dbo.DimCustomerTemp
+FOR SYSTEM_TIME BETWEEN 'START DATE' AND 'END DATE'
 WHERE [CustomerId] = 101
 ORDER BY [validFrom] DESC;
