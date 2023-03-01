088c1bcf7d07fce8ffcce6ed5490a40927b0230e
nareyes <nareyes@protonmail.com>
2023-03-01 15:53:44 -0600

    Add File via Git

---
+++ azure_data_factory_course/sql_files/create_employee.sql
@@ -0,0 +1,14 @@
+CREATE TABLE dbo.Employee (
+    Name    VARCHAR(MAX),
+    Gender  VARCHAR(MAX),
+    Dept    VARCHAR(MAX)
+);
+
+
+INSERT INTO dbo.Employee(Name, Gender, Dept) VALUES ('Tanmoy', 'M', 'IT');
+INSERT INTO dbo.Employee(Name, Gender, Dept) VALUES ('Roop', 'M', 'HR');
+INSERT INTO dbo.Employee(Name, Gender, Dept) VALUES ('Malathy', 'F', 'IT');
+INSERT INTO dbo.Employee(Name, Gender, Dept) VALUES ('Kranthi', 'M', 'HR');
+INSERT INTO dbo.Employee(Name, Gender, Dept) VALUES ('Bhuvi', 'F', 'Payrole');
+
+SELECT * FROM dbo.Employee;
