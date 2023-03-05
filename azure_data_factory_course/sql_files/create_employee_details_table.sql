ee27cc5bb05250888aa59a3c6e01dae4d432905a
nareyes <nareyes@protonmail.com>
2023-03-05 10:39:01 -0600

    Add table creation scripts

---
+++ azure_data_factory_course/sql_files/create_employee_details_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE dbo.EmployeeDetails (
+    Emp_Id      VARCHAR(MAX)
+    , Name      VARCHAR(MAX)
+    , Gender	VARCHAR(MAX)
+    , Salary    VARCHAR(MAX)
+    , File_Name VARCHAR(MAX)
+
+);
+
+SELECT * FROM dbo.EmployeeDetails;
---
+++ azure_data_factory_course/sql_files/create_sales_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE dbo.Sales (
+    Customer        VARCHAR(MAX)
+    , Gender        VARCHAR(MAX)
+    , Product_Code	VARCHAR(MAX)
+    , Price         VARCHAR(MAX)
+    , File_Name     VARCHAR(MAX)
+
+);
+
+SELECT * FROM dbo.Sales;
