934b5ca8e29ae8ad2b86e861335f31ff8b6eddcd
nareyes <nareyes@protonmail.com>
2023-03-05 11:44:42 -0600

    Add sql files

---
+++ azure_data_factory_course/sql_files/create_fault_tolerance_demo_table.sql
@@ -0,0 +1,10 @@
+-- Fault Tolerance Demo
+CREATE TABLE dbo.EmployeeDetailsFault (
+    Name      VARCHAR(8)
+    , Gender  CHAR(1)
+    , Salary  INT
+    , Dept    CHAR(1)
+    , DOJ     DATE
+);
+
+SELECT * FROM dbo.EmployeeDetailsFault;
