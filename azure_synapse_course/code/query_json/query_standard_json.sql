5fb3099e7e6e3ddb7716ef3ea31719f5257438c1
nareyes <nareyes@protonmail.com>
2023-05-15 15:54:49 -0500

    add json query scripts

---
+++ azure_synapse_course/code/query_json/query_standard_json.sql
@@ -0,0 +1,27 @@
+USE NYC_Taxi_Serverless
+GO
+
+-- Query Standard JSON Values w/ OPENJSON
+-- OPENJSON = More Efficient, More Functionality
+SELECT
+    RateCodeID
+    ,RateCodeDescription
+FROM
+    OPENROWSET(
+        BULK 'rate_code.json',
+        DATA_SOURCE = 'NYC_Taxi_Raw',
+        FORMAT = 'CSV', -- Even for JSON
+        PARSER_VERSION = '1.0',
+        FIELDTERMINATOR = '0x0b',
+        FIELDQUOTE = '0x0b',
+        ROWTERMINATOR = '0x0b' -- Vertical Tab
+    ) 
+    WITH(
+        jsonDoc NVARCHAR(MAX)
+    ) AS RateCode
+
+CROSS APPLY OPENJSON(jsonDoc) -- Return Key Value Pairs
+    WITH(
+        RateCodeID              TINYINT     '$.rate_code_id'
+        ,RateCodeDescription    VARCHAR(20) '$.rate_code'
+    )
