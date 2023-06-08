5c7934db4f0d9ab725ef09573fbd3f3950a54f40
nareyes <nareyes@protonmail.com>
2023-06-08 13:38:35 -0500

    add ch11 code

--- fundamental_sql_server/code/ch11_programmable_objects.sql
+++ fundamental_sql_server/code/ch11_programmable_objects.sql
@@ -59,3 +59,122 @@ FROM HR.Employees
 WHERE empid = 3;
 
 SELECT @firstname AS firstname, @lastname AS lastname;
+
+
+-------------
+-- BATCHES --
+-------------
+
+-- Valid batch
+PRINT 'First batch';
+GO
+
+
+-- Invalid batch
+PRINT 'Second batch';
+SELECT custid FROM Sales.Customers;
+SELECT orderid FOM Sales.Orders;
+GO
+
+
+-- Valid batch
+PRINT 'Third batch';
+SELECT empid FROM HR.Employees;
+
+
+-- Batches and Variables
+DECLARE @i AS INT;
+SET @i = 10;
+
+PRINT @i;
+GO -- Succeeds
+
+PRINT @i; -- Fails
+
+
+------------------
+-- CONTROL FLOW --
+------------------
+
+-- Basic Control Flow
+IF YEAR(SYSDATETIME()) <> YEAR(DATEADD(day, 1, SYSDATETIME()))
+    PRINT 'Today is the last day of the year.';
+ELSE
+    PRINT 'Today is not the last day of the year.';
+
+
+-- Nested Control Flow
+IF YEAR(SYSDATETIME()) <> YEAR(DATEADD(day, 1, SYSDATETIME()))
+    PRINT 'Today is the last day of the year.';
+ELSE
+    IF MONTH(SYSDATETIME()) <> MONTH(DATEADD(day, 1, SYSDATETIME()))
+        PRINT 'Today is the last day of the month but not the last day of the year.';
+    ELSE
+        PRINT 'Today is not the last day of the month.';
+
+
+-- Control Flow w/ Statement Blocks
+IF DAY(SYSDATETIME()) = 1
+
+	BEGIN
+        PRINT 'Today is the first day of the month.';
+        PRINT 'Starting first-of-month-day process.';
+        /* ... process code goes here ... */
+        PRINT 'Finished first-of-month-day database process.';
+	END;
+	
+ELSE
+	BEGIN
+        PRINT 'Today is not the first day of the month.';
+        PRINT 'Starting non-first-of-month-day process.';
+        /* ... process code goes here ... */
+        PRINT 'Finished non-first-of-month-day process.';
+	END;
+
+
+-- While Loop
+DECLARE @i AS INT = 1;
+WHILE @i <= 10
+BEGIN
+	PRINT @i;
+	SET @i = @i + 1;
+END;
+
+
+-- While Loop w/ Break
+DECLARE @i AS INT = 1;
+WHILE @i <= 10
+BEGIN
+	IF @i = 6 BREAK;
+	PRINT @i;
+	SET @i = @i + 1;
+END;
+
+
+-- While Loop w/ Continue
+DECLARE @i AS INT = 0;
+WHILE @i < 10
+BEGIN
+	SET @i = @i + 1;
+	IF @i = 6 CONTINUE;
+	PRINT @i;
+END;
+
+
+-- While Loop Example for Numbers Table
+SET NOCOUNT ON;
+
+DROP TABLE IF EXISTS dbo.Numbers;
+
+CREATE TABLE dbo.Numbers(n INT NOT NULL PRIMARY KEY);
+GO
+
+DECLARE @i AS INT = 1;
+
+WHILE @i <= 1000
+BEGIN
+	INSERT INTO dbo.Numbers(n) VALUES(@i);
+	SET @i = @i + 1;
+END;
+
+SELECT * FROM dbo.Numbers;
