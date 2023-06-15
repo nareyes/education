125ae68d9de1c2c98af9f9e28a086fa977c904e5
nareyes <nareyes@protonmail.com>
2023-06-15 07:02:54 -0500

    update course files

--- azure_synapse_course/code/_docs/admin/kill_session.sql
+++
@@ -1,9 +0,0 @@
--- Query Session ID
-SELECT DB_NAME(database_id), 'kill '+ CAST(session_id AS VARCHAR(10)), *
-    FROM sys.dm_exec_sessions
-    WHERE DB_NAME(database_id) NOT IN ('master')
-    ORDER BY 1;
-
-
--- Kill Session ID
-KILL session_id;
