2cb0c5957977232c8a58c3f5dff314ff8add48a1
nareyes <nreyes0985@gmail.com>
2024-02-08 11:04:55 -0600

    update python course

--- python_developer_course/projects/todo_application/cli.py
+++ python_developer_course/projects/todo_application/cli.py
@@ -2,167 +2,41 @@
 # PYTHON VERSION 3.11.5
 
 import module
-
-# VARIABLES & PROMPTS
-user_input_action = "Type Add, Show, Edit, Complete, Clear or Exit: "
-user_input_todo = "Enter a Todo: "
-user_input_position = "Enter a Valid Todo Number to Edit: "
-user_input_new = "Enter a New Item: "
-user_input_complete = "Enter a Valid Todo Number to Complete: "
-todos = []
-
-
-# FUNCTIONS
-def read_todos():
-    """
-    Reads the todo items from the "todos.txt" file.
-    Returns a list containing the todo items.
-    """
-    with open("data/todos.txt", "r") as file_local:
-        todos_local = file_local.readlines()
-
-    return todos_local
-
-
-def add_todos():
-    """
-    Adds a new todo item to the list.
-    Prompts the user to enter a new todo item.
-    Writes the updated list back to the "todos.txt" file.
-    Prints a message confirming the addition.
-    """
-    todo = input(user_input_todo).strip().title() + "\n"
-    todos = read_todos()
-    todos.append(todo)
-
-    with open("data/todos.txt", "w") as file:
-        file.writelines(todos)
-
-    message = f"Added: {todo}"
-    print(message)
-
-
-def show_todos():
-    """
-    Displays the current list of todo items.
-    If there are no todos, prints "No Pending Todos".
-    Otherwise, prints each todo item along with its index.
-    """
-    todos = read_todos()
-    if todos == []:
-        print('No Pending Todos')
-    else:
-        for index, item in enumerate(todos):
-            item = item.strip("\n")
-            output = f"{index + 1}: {item}"
-            print(output)
-
-
-def edit_todos():
-    """
-    Allows the user to edit an existing todo item.
-    Prompts the user to enter the position of the todo item they want to edit.
-    Allows the user to enter a new todo item to replace the old one.
-    Writes the updated todos back to the "todos.txt" file.
-    Prints a message confirming the edit.
-    """
-    todos = read_todos()
-    position = int(input(user_input_position).strip())
-    old_todo = todos[position - 1]
-
-    if position >= 1 and position <= len(todos):
-        new_todo = input(user_input_new).strip().title()
-        todos[position - 1] = new_todo + "\n"
-    else:
-        while position < 1 or position > len(todos):
-            position = int(input(user_input_position).strip())
-
-            if position >= 1 and position <= len(todos):
-                new_todo = input(user_input_new).strip().title()
-                todos[position - 1] = new_todo + "\n"
-
-    with open("data/todos.txt", "w") as file:
-        file.writelines(todos)
-
-    message = f"Replaced: {old_todo} With: {new_todo}"
-    print(message)
-
-
-def complete_todos():
-    """
-    Marks a todo item as completed.
-    Prompts the user to enter the position of the todo item they want to mark as completed.
-    Removes the completed todo item from the list.
-    Writes the updated todos back to the "todos.txt" file.
-    Prints a message confirming the completion.
-    """
-    todos = read_todos()
-    position = int(input(user_input_complete).strip())
-    completed_todo = todos[position - 1]
-
-    if position >= 1 and position <= len(todos):
-        todos.pop(position - 1)
-    else:
-        while position < 1 or position > len(todos):
-            position = int(input(user_input_complete).strip())
-
-            if position >= 1 and position <= len(todos):
-                todos.pop(position - 1)
-
-    with open("data/todos.txt", "w") as file:
-        file.writelines(todos)
-
-    message = f"Completed Todo: {completed_todo}"
-    print(message)
-
-
-def clear_todos():
-    """
-    Clears all todos from the list.
-    Opens the "todos.txt" file in write mode and immediately closes it.
-
-    """
-    open("data/todos.txt", "w").close()
-
-    message = "All Todos Cleared\n"
-    print(message)
-
+todo_app = module.TodoList()
 
 # APPLICATION LOGIC
 # - The program runs in a continuous loop, prompting the user for an action (add, show, edit, complete, clear, or exit).
-# - Depending on the action, it calls the corresponding function.
+# - Depending on the action, it calls the corresponding method of the TodoList class.
 # - If the action is 'edit' or 'complete', it handles IndexError exceptions if the user provides an invalid todo number.
 # - If the action is 'exit', the loop breaks, terminating the program.
 # - If the user inputs an invalid action, it prints "Invalid Entry".
 # - After the loop terminates, it prints "Program Terminated".
 while True:
-    user_action = input(user_input_action).strip().lower()
+    user_action = input(todo_app.user_input_action).strip().lower()
 
-    match user_action:
-        case "add":
-            add_todos()
-        case "show":
-            show_todos()
-        case "edit":
-            try:
-                show_todos()
-                edit_todos()
-            except IndexError:
-                print("Invalid Entry")
-                continue
-        case "complete":
-            try:
-                show_todos()
-                complete_todos()
-            except IndexError:
-                print("Invalid Entry")
-                continue
-        case "clear":
-            clear_todos()
-        case "exit":
-            break
-        case _:
-            print("Invalid Entry")
+    if user_action == "add":
+        todo_app.add_todos()
+    elif user_action == "show":
+        todo_app.show_todos()
+    elif user_action == "edit":
+        try:
+            todo_app.show_todos()
+            todo_app.edit_todos()
+        except IndexError:
+            print("Invalid Entry\n")
+            continue
+    elif user_action == "complete":
+        try:
+            todo_app.show_todos()
+            todo_app.complete_todos()
+        except IndexError:
+            print("Invalid Entry\n")
+            continue
+    elif user_action == "clear":
+        todo_app.clear_todos()
+    elif user_action == "exit":
+        break
+    else:
+        print("Invalid Entry\n")
 
-
-print("Program Terminated")
+print("Program Terminated\n")
--- python_developer_course/projects/todo_application/module.py
+++ python_developer_course/projects/todo_application/module.py
@@ -1,86 +1,124 @@
-# variables
-user_input_action = "Type Add, Show, Edit, Complete, Clear or Exit: "
-user_input_todo = "Enter a Todo: "
-user_input_position = "Enter a Valid Todo Number to Edit: "
-user_input_new = "Enter a New Item: "
-user_input_complete = "Enter a Valid Todo Number to Complete: "
-todos = []
+# TO DO LIST CLI APPLICATION
+# PYTHON VERSION 3.11.5
 
 class TodoList:
     def __init__(self):
+        """
+        Initializes a TodoList object.
+        Sets up default user input prompts, file path for storing todos, and initializes an empty list to store todos.
+        """
+        self.user_input_action = "Type Add, Show, Edit, Complete, Clear or Exit: "
+        self.user_input_todo = "Enter a Todo: "
+        self.user_input_position = "Enter a Valid Todo Number to Edit: "
+        self.user_input_new = "Enter a New Item: "
+        self.user_input_complete = "Enter a Valid Todo Number to Complete: "
+        self.filepath = "data/todos.txt"
         self.todos = []
-    
-    def read_todos(self):
-        with open("data/todos.txt", "r") as file_local:
-            todos_local = file_local.readlines()
 
+    def read_todos(self):
+        """
+        Reads the todo items from the "todos.txt" file.
+        Returns a list containing the todo items.
+        """
+        with open(self.filepath, "r") as file_local:
+            todos_local = file_local.readlines()
         return todos_local
 
     def add_todos(self):
-        todo = input(user_input_todo).strip().title() + "\n"
-        todos = read_todos()
+        """
+        Adds a new todo item to the list.
+        Prompts the user to enter a new todo item.
+        Writes the updated list back to the "todos.txt" file.
+        Prints a message confirming the addition.
+        """
+        todo = input(self.user_input_todo).strip().title() + "\n"
+        todos = self.read_todos()
         todos.append(todo)
 
-        with open("data/todos.txt", "w") as file:
+        with open(self.filepath, "w") as file:
             file.writelines(todos)
 
         message = f"Added: {todo}"
         print(message)
 
     def show_todos(self):
-        todos = read_todos()
+        """
+        Displays the current list of todo items.
+        If there are no todos, prints "No Pending Todos".
+        Otherwise, prints each todo item along with its index.
+        """
+        todos = self.read_todos()
         if todos == []:
-            print('No Pending Todos')
+            print("No Pending Todos\n")
         else:
             for index, item in enumerate(todos):
                 item = item.strip("\n")
                 output = f"{index + 1}: {item}"
                 print(output)
+        print("\n")
 
     def edit_todos(self):
-        todos = read_todos()
-        position = int(input(user_input_position).strip())
+        """
+        Allows the user to edit an existing todo item.
+        Prompts the user to enter the position of the todo item they want to edit.
+        Allows the user to enter a new todo item to replace the old one.
+        Writes the updated todos back to the "todos.txt" file.
+        Prints a message confirming the edit.
+        """
+        todos = self.read_todos()
+        position = int(input(self.user_input_position).strip())
         old_todo = todos[position - 1]
 
         if position >= 1 and position <= len(todos):
-            new_todo = input(user_input_new).strip().title()
+            new_todo = input(self.user_input_new).strip().title()
             todos[position - 1] = new_todo + "\n"
         else:
             while position < 1 or position > len(todos):
-                position = int(input(user_input_position).strip())
+                position = int(input(self.user_input_position).strip())
 
                 if position >= 1 and position <= len(todos):
-                    new_todo = input(user_input_new).strip().title()
+                    new_todo = input(self.user_input_new).strip().title()
                     todos[position - 1] = new_todo + "\n"
 
-        with open("data/todos.txt", "w") as file:
+        with open(self.filepath, "w") as file:
             file.writelines(todos)
 
-        message = f"Replaced: {old_todo} With: {new_todo}"
+        message = f"Replaced: {old_todo} With: {new_todo}\n"
         print(message)
 
     def complete_todos(self):
-        todos = read_todos()
-        position = int(input(user_input_complete).strip())
+        """
+        Marks a todo item as completed.
+        Prompts the user to enter the position of the todo item they want to mark as completed.
+        Removes the completed todo item from the list.
+        Writes the updated todos back to the "todos.txt" file.
+        Prints a message confirming the completion.
+        """
+        todos = self.read_todos()
+        position = int(input(self.user_input_complete).strip())
         completed_todo = todos[position - 1]
 
         if position >= 1 and position <= len(todos):
             todos.pop(position - 1)
         else:
             while position < 1 or position > len(todos):
-                position = int(input(user_input_complete).strip())
+                position = int(input(self.user_input_complete).strip())
 
                 if position >= 1 and position <= len(todos):
                     todos.pop(position - 1)
 
-        with open("data/todos.txt", "w") as file:
+        with open(self.filepath, "w") as file:
             file.writelines(todos)
 
         message = f"Completed Todo: {completed_todo}"
         print(message)
 
     def clear_todos(self):
-        open("data/todos.txt", "w").close()
+        """
+        Clears all todos from the list.
+        Opens the "todos.txt" file in write mode and immediately closes it.
+        """
+        open(self.filepath, "w").close()
 
         message = "All Todos Cleared\n"
         print(message)
