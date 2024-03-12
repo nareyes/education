f0e3dd1d2dbc984c102f255288fbe37c08fd3857
nareyes <nreyes0985@gmail.com>
2024-03-12 05:30:21 -0500

    update python course

--- python_developer_course/pdf_generator/main.py
+++ python_developer_course/pdf_generator/main.py
@@ -1,8 +1,20 @@
+"""
+Script to generate a PDF document with cover page and topics with multiple pages.
+
+Dependencies:
+    - fpdf: For PDF generation
+    - pandas: For reading CSV file
+
+The script reads data from a CSV file named 'topics.csv' with columns ['Topic', 'Pages'].
+Each topic in the CSV file will have its own page(s) in the generated PDF.
+"""
+
 from fpdf import FPDF
 import pandas as pd
 
 # create blank pdf 
 pdf = FPDF(orientation = 'P', unit = 'mm', format = 'A4')
+pdf.set_auto_page_break(auto = False, margin = 0)
 
 
 # import topics csv
@@ -32,10 +44,28 @@ pdf.text(x, y, "Python Developer Course Notes")
 
 # add topics as pages starting on second page
 for index, row in df.iterrows():
-    pdf.add_page()
-    pdf.set_font(family = 'Times', style = 'B', size = 24)
-    pdf.cell(w = 0, h = 12, txt = row['Topic'], align = 'L', ln = 1, border = 0)
-    pdf.line(x1 = 10, x2 = 200, y1 = 20, y2 = 20)
+    # pdf.add_page()
+    # pdf.set_font(family = 'Times', style = 'B', size = 24)
+    # pdf.cell(w = 0, h = 12, txt = row['Topic'], align = 'L', ln = 1, border = 0)
+    # pdf.line(x1 = 10, x2 = 200, y1 = 20, y2 = 20)
+
+    for i in range(row['Pages']):
+        pdf.add_page()
+        pdf.set_font(family = 'Times', style = 'B', size = 24)
+        pdf.set_text_color(0, 0, 0)
+        pdf.cell(w = 0, h = 12, txt = row['Topic'] + ' (Page ' + str(i + 1) + ')', align = 'L', ln = 1, border = 0)
+        pdf.line(x1 = 10, x2 = 200, y1 = 20, y2 = 20)
+
+        # lines
+        for y in range(30, 290, 10):
+            pdf.line(x1 = 10, x2 = 200, y1 = y, y2 = y)
+
+        # footer
+        pdf.ln(260)
+        pdf.set_font(family = 'Times', style = 'I', size = 8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w = 0, h = 10, txt = row['Topic'], align = 'R')
+
 
 
 # output pdf
